a33c77eaf311d2563f6282f9c44f7f7b776dfdef
SlimySlime <slime.mana.hk@gmail.com>
2022-06-13 23:29:55 +0900

    Obstacle Test
    敵キャラの基本部分の実装

---
+++ Assets/SlimySlime/Scripts/Ghost.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : Obstacle
+{
+    [SerializeField]private float moveSpeed;
+    private Rigidbody2D rb;
+    private Transform girl;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRealObstacle = false;
+        rb = GetComponent<Rigidbody2D>();
+        girl = GameObject.FindWithTag("Girl").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //èóÇÃéqÇí«Ç¢Ç©ÇØÇÈ
+        if (!attackingGirl)
+        {
+            rb.position += moveSpeed * Time.deltaTime * (Vector2)(girl.position - this.transform.position).normalized;
+        }
+    }
+}
