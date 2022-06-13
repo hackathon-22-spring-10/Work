234cfce28b4408e3aff00525e2699ab8ee807924
guy7cc <alrdy.tree1sawa@gmail.com>
2022-06-13 23:34:17 +0900

    Create GhostPlayer

---
+++ Assets/guy7cc/Scripts/GhostPlayer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostPlayer : Player
+{
+    private SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        base.Start();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (active) Move();
+    }
+
+    private void Move()
+    {
+        float x = 0;
+        float y = 0;
+        if (Input.GetKey(config.left)) x -= xSpeed;
+        if (Input.GetKey(config.right)) x += xSpeed;
+        if (Input.GetKey(config.down)) y -= ySpeed;
+        if (Input.GetKey(config.jump)) y += ySpeed;
+        rigid.velocity = new Vector2(x, y);
+        
+    }
+}
