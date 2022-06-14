3a2bd186793f81c5f410c1080d6ac40505c6d257
kamecha <masakikameyama81@gmail.com>
2022-06-14 21:05:17 +0900

    円運動の追加

--- Assets/kamecha/Scripts/Girl.cs
+++ Assets/kamecha/Scripts/Girl.cs
@@ -9,6 +9,7 @@ public class Girl : MonoBehaviour
     public float hitPoint { get; set; } = maxHitPoint;
     public float favorabilityRating { get; set; } = 0;
     private Rigidbody2D rb;
+    public bool flagTest;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +19,20 @@ public class Girl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        rotate(3.0f, flagTest);
     }
-
-    void FixedUpdate()
-    {
-        girlMove();
-    }
-
     void girlMove()
     {
         rb.velocity = new Vector2(speed, 0);
+    }
+    // Unityのx,y軸で半径radiusで正の方向に回転
+    // inversionFlag == true -> 負の方向に回転
+    public void rotate(float radius, bool inversionFlag)
+    {
+        float vx = radius * Mathf.Sin(Time.time * speed);
+        vx = -vx;
+        float vy = radius * Mathf.Cos(Time.time * speed);
+        vy = (inversionFlag) ? -vy : vy;
+        rb.velocity = new Vector2(vx, vy);
     }
 }
