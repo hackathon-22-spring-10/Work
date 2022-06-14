4bcbfcff9dde26e5c2208ffc32189b33e533b15b
kamecha <masakikameyama81@gmail.com>
2022-06-14 21:33:30 +0900

    波形の移動とそれぞれの連携の追加

--- Assets/kamecha/Scripts/Girl.cs
+++ Assets/kamecha/Scripts/Girl.cs
@@ -16,23 +16,47 @@ public class Girl : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rotate(3.0f, flagTest);
+        // rotate(3.0f, Mathf.PI/2, flagTest);
+        patternMove(Random.Range(0, 3));
     }
-    void girlMove()
+    void right()
     {
         rb.velocity = new Vector2(speed, 0);
     }
     // Unityのx,y軸で半径radiusで正の方向に回転
     // inversionFlag == true -> 負の方向に回転
-    public void rotate(float radius, bool inversionFlag)
+    public void rotate(float radius, float theta, bool inversionFlag)
     {
-        float vx = radius * Mathf.Sin(Time.time * speed);
+        float vx = radius * Mathf.Sin(Time.time * speed + theta);
         vx = -vx;
-        float vy = radius * Mathf.Cos(Time.time * speed);
+        float vy = radius * Mathf.Cos(Time.time * speed + theta);
         vy = (inversionFlag) ? -vy : vy;
         rb.velocity = new Vector2(vx, vy);
+    }
+
+    public void wave(float radius) 
+    {
+        float vx = speed;
+        float vy = radius * Mathf.Sin(Time.time * speed);
+        rb.velocity = new Vector2(vx, vy);
+    }
+
+    public void patternMove(int pattern) {
+        switch (pattern)
+        {
+            case 0:
+            right();
+            break;
+            case 1:
+            rotate(3.0f, Mathf.PI/2, false);
+            break;
+            case 2:
+            wave(2.0f);
+            break;
+            default:
+            break;
+        }
     }
 }
